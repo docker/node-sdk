4badac82b40c6a8673ef350d607c26f8401d6303
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2020-07-01 11:39:10 -0400

    Rebuild the source
    Signed-off-by: Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>

--- dist/containers.d.ts
+++ dist/containers.d.ts
@@ -1,1 +1,1 @@
-export { ExecRequest, ExecResponse, ListRequest, ListResponse, LogsRequest, LogsResponse, RunRequest, RunResponse, StopRequest, StopResponse, DeleteRequest, DeleteResponse, Container, Port, } from './protos/containers/v1/containers_pb';
+export { ExecRequest, ExecResponse, InspectRequest, InspectResponse, ListRequest, ListResponse, LogsRequest, LogsResponse, RunRequest, RunResponse, StopRequest, StopResponse, DeleteRequest, DeleteResponse, Container, Port, } from './protos/containers/v1/containers_pb';
--- dist/containers.js
+++ dist/containers.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var containers_pb_1 = require("./protos/containers/v1/containers_pb");
 Object.defineProperty(exports, "ExecRequest", { enumerable: true, get: function () { return containers_pb_1.ExecRequest; } });
 Object.defineProperty(exports, "ExecResponse", { enumerable: true, get: function () { return containers_pb_1.ExecResponse; } });
+Object.defineProperty(exports, "InspectRequest", { enumerable: true, get: function () { return containers_pb_1.InspectRequest; } });
+Object.defineProperty(exports, "InspectResponse", { enumerable: true, get: function () { return containers_pb_1.InspectResponse; } });
 Object.defineProperty(exports, "ListRequest", { enumerable: true, get: function () { return containers_pb_1.ListRequest; } });
 Object.defineProperty(exports, "ListResponse", { enumerable: true, get: function () { return containers_pb_1.ListResponse; } });
 Object.defineProperty(exports, "LogsRequest", { enumerable: true, get: function () { return containers_pb_1.LogsRequest; } });
