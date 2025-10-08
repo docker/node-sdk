cb15f66a9448b53303111c6d1cb39bd2af24840a
Luke Bunselmeyer <william.bunselmeyer@docker.com>
2025-10-08 13:06:06 -0400

    Updated build packaging - Migrated to `tsdown` from `tsup` b/c `tsup` is no longer actively maintained. - Generated dts sourcemaps to link generated types to canonical source types in `lib`. - removed `./dist/*` from package.json `exports`.

--- test-integration/esm-project/import.test.ts
+++ test-integration/esm-project/import.test.ts
@@ -3,7 +3,7 @@ import assert from 'node:assert/strict';
 
 import { DockerClient } from '@docker/node-sdk';
 
-test('ES module import should work', () => {
+test('ES module should import correctly', () => {
     assert.equal(typeof DockerClient, 'function');
     assert.equal(typeof DockerClient.fromDockerConfig, 'function');
 });
