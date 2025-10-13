63a17e2ed374cc594c5ecf96a35f6f6baca8103c
Luke Bunselmeyer <william.bunselmeyer@docker.com>
2025-10-13 16:18:01 -0400

    Limit docker socket proxy to 127.0.0.1

--- bake_test_all.sh
+++ bake_test_all.sh
@@ -8,5 +8,5 @@ function tearDown {
     docker stop docker-sock-for-bake-test-all
 }
 trap tearDown EXIT ERR
-docker run --rm -d --name docker-sock-for-bake-test-all -p 2375:2375 -v /var/run/docker.sock:/var/run/docker.sock alpine/socat tcp-listen:2375,reuseaddr,fork unix-connect:/var/run/docker.sock
+docker run --rm -d --name docker-sock-for-bake-test-all -p 127.0.0.1:2375:2375 -v /var/run/docker.sock:/var/run/docker.sock alpine/socat tcp-listen:2375,reuseaddr,fork unix-connect:/var/run/docker.sock
 docker buildx bake test-all
